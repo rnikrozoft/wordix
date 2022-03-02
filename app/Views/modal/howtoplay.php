9b404affb8394b855a5081335703ae6ddc5ee0f8
rnikrozoft <rnikrozoft.dev@gmail>
2022-03-03 00:20:20 +0700

    create modal for howtoplay

---
+++ app/Views/modal/howtoplay.php
@@ -0,0 +1,28 @@
+<div class="modal fade" id="howtoplay" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered mx-wd-sm-650" role="document">
+        <div class="modal-content bd-0 bg-transparent">
+            <div class="modal-body pd-0">
+                <a href="javascript:;" role="button" class="close pos-absolute t-15 r-15 z-index-10" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </a>
+
+                <div class="row no-gutters">
+                    <div class="col-3 col-sm-5 col-md-6 col-lg-5 bg-primary rounded-left">
+                        <div class="wd-100p ht-100p">
+                            <img src="https://via.placeholder.com/286x429" class="wd-100p img-fit-cover img-object-top rounded-left" alt="">
+                        </div>
+                    </div>
+                    <div class="col-9 col-sm-7 col-md-6 col-lg-7 bg-white rounded-right">
+                        <div class="ht-100p d-flex flex-column justify-content-center pd-20 pd-sm-30 pd-md-40">
+                            <span class="tx-color-04"><i data-feather="bar-chart-2" class="wd-40 ht-40 stroke-wd-3 mg-b-20"></i></span>
+                            <h3 class="tx-16 tx-sm-20 tx-md-24 mg-b-15 mg-md-b-20">First Visit Metrics</h3>
+                            <p class="tx-14 tx-md-16 tx-color-02">Tracks how users are finding your website, such as organic search or social media, and how engaged they are with your website.</p>
+                            <p class="tx-12 tx-md-13 tx-color-03 mg-b-25">Ei quo quas vocent. Vel libris luptatum ut, ex mel graeci comprehensam, ut doming antiopam tincidunt sed. Quis efficiantur vix eu, ne eum quas antiopam, ex fugit atqui mel...</p>
+                            <a href="" class="btn btn-primary btn-block btn-uppercase">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
